21b8c6bb807cb4fc50c48bf01cfad834901defd4
Aliyu Kamilu <aliyukamilu002@gmail.com>
2022-10-29 15:02:44 +0100

    first commit & project folder setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,9 @@
 import React from "react";
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  const [data, setData] = React.useState(null);
-  const [pingmess, setPingmess] = React.useState(null);
-  
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data.message)
-      });
-
-
-    fetch("/pingMes")
-      .then((res) => res.json())
-      .then((data) => {
-        setPingmess(data.message)
-      });
-  }, []);
-
-  // function pingAPi() {
-  //   fetch("/api")
-  //     .then((res) => res.json())
-  //     .then((data) => setData(data.message));
-  // }
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <p>{!data ? "Loading..." : data}</p>
-        <p>{!pingmess ? "Loading ping Response..." : pingmess}</p>
-        {/* <button onClick={pingAPi}>Ping Api</button> */}
-      </header>
+      <p>Hello Boss</p>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,54 +1,7 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
-let date = new Date().getMilliseconds();
 const axios = require('axios');
-
-
-var minutes = 0.1, the_interval = minutes * 60 * 1000;
-const URL = 'https://auth-rock-and-art.herokuapp.com/api/auth/register/';
-
-function pingFirstServer() {
-  date = date + 1
-  axios.get('http://localhost:5000/ping')
-    .then(response => {
-      console.log(response.data);
-
-      app.get("/api", (req, res) => {
-        res.json({ message: 'On premise server Pinged Server Connected' });
-      });
-      postAm()
-
-      setInterval(postAm, the_interval);
-    }).catch(err => {
-      console.error(err)
-      res.json({ message: `Their's an error. server not pinged, server is down.` });
-    });
-}
-pingFirstServer();
-
-
-function postAm() {
-  date = date + 1
-  axios.post(URL, {
-    first_name: "Aliyu " + date,
-    last_name: "foo " + date,
-    password: "passsj" + date,
-    email: "bar@gmail.com" + date
-
-  }).then(response => {
-    console.log(response.data);
-
-    app.get("/pingMes", (req, res) => {
-      res.json({ message: 'Server sent a report' });
-    });
-  })
-    .catch(err => console.error(err));
-}
-
-
-
-
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
